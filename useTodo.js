a15e17fbb7c306ad82c60154bffe97fdefe4e789
Alexis-AL <alexis.alara@alumnos.udg.mx>
2023-06-14 13:52:17 -0500

    primer commit

---
+++ useTodo.js
@@ -0,0 +1,58 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+export const useTodo = () => {
+    const initialState = [
+        //     {
+        //     id: new Date().getTime(),
+        //     description: 'Recolectar la gema del alma',
+        //     done: false
+        // },
+        // {
+        //     id: new Date().getTime()*3,
+        //     description: 'Recolectar la gema de la mente',
+        //     done: false
+        // }
+    ]
+        const init = () =>{
+            return JSON.parse(localStorage.getItem('todos')) || [];
+        }
+    
+        const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init)
+    
+        useEffect(() => {
+          localStorage.setItem('todos', JSON.stringify(todos) )
+        }, [todos])
+        
+    
+        const handleNewTodo = (todo) => {
+            const action = {
+                type: '[TODO] Add Todo',
+                payload: todo
+            }
+    
+            dispatchTodo( action )
+        }
+    
+        const handleDeleteTodo = (id) => {
+            dispatchTodo({
+                type: '[TODO] Remove Todo',
+                payload: id
+            })
+        }
+    
+        const handleToggleTodo = (id) => {
+            dispatchTodo({
+                type: '[TODO] Toggle Todo',
+                payload: id
+            })
+        }
+    return{
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo=>!todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo
+    }
+}
